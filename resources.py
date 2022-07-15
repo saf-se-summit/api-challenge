2f365f25e1aa68519f15d6fc9cbd70ab3afa91b6
oumao <evansotis2015@gmail.com>
2022-07-15 17:56:13 +0300

    Resources

---
+++ resources.py
@@ -0,0 +1,21 @@
+from flask_restful import Resource
+
+class Customer(Resource):
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass 
+
+    def delete(self):
+        pass 
+
+
+class CustomerList(Resource):
+
+    def get(self):
+        pass 
+
+    def post(self):
+        pass 
